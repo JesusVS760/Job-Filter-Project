a812a2d357f82493078f02be9f992cd1159106b0
JesusVA760 <tonio4340406@gmail.com>
2024-08-15 14:55:59 -0700

    json display

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,18 @@ import React, { useState } from "react";
 import "./App.css";
 import headerImage from "../images/bg-header-desktop.svg";
 import Card from "./components/Card";
-import Company1 from "../images/photosnap.svg";
 import json from "../data.json";
 const App = () => {
   const [data, setData] = useState(json);
-
+  console.log(data);
   return (
     <div className="app-container">
       <div className="app-style-image">
         <img src={headerImage} alt="header image" />
       </div>
-      <Card company={Company1} position={json[0].position} />
+      {data.map((job) => (
+        <Card key={job.id} job={job} />
+      ))}
     </div>
   );
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,34 +1,34 @@
 import React from "react";
 import "./Card.css";
 
-const Card = ({ company, position }) => {
-  console.log(position);
+const Card = ({ job }) => {
   return (
     <div className="card-container">
       <div className="card-header">
         <div className="card-profile-img">
-          <img src={company} alt="profile image" />
+          <img src={job.logo} alt="profile image" />
         </div>
         <div className="card-content">
           <div className="profile-company-info">
-            <h3 className="company-name"></h3>
-            <h3 className="new">new</h3>
-            <h3 className="featured">featured</h3>
+            <h3 className="company-name">{job.company}</h3>
+            <h3 className="new">{job.new ? "NEW!" : ""}</h3>
+            <h3 className="featured">{job.featured ? "FEATURED" : ""}</h3>
           </div>
+
           <div className="comapny-job-position">
-            <h2 className="job-title">{position}</h2>
+            <h2 className="job-title">{job.position}</h2>
           </div>
           <div className="card-conditions">
-            <p>1d ago</p>
-            <p>Part Time</p>
-            <p>Remote</p>
+            <p>{job.postedAt}</p>
+            <p>{job.contract}</p>
+            <p>{job.location}</p>
           </div>
         </div>
       </div>
       <div className="card-skills-filter">
-        <p>Frontend</p>
-        <p>Senior</p>
-        <p>HTML</p>
+        <button>{job.role}</button>
+        <button>{job.level}</button>
+        <button>{job.languages}</button>
       </div>
     </div>
   );
