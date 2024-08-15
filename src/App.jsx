614c0bc263dc4a318d8b8605174391cdf01c8c67
JesusVA760 <tonio4340406@gmail.com>
2024-08-14 18:16:52 -0700

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import headerImage from "../images/bg-header-desktop.svg";
-
+import Card from "./components/Card";
+import Company1 from "../images/photosnap.svg";
+import json from "../data.json";
 const App = () => {
+  const [data, setData] = useState(json);
+
   return (
     <div className="app-container">
       <div className="app-style-image">
         <img src={headerImage} alt="header image" />
       </div>
+      <Card company={Company1} position={json[0].position} />
     </div>
   );
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import "./Card.css";
 
-const Card = () => {
+const Card = ({ company, position }) => {
+  console.log(position);
   return (
     <div className="card-container">
       <div className="card-header">
         <div className="card-profile-img">
-          <img src="" alt="profile image" />
+          <img src={company} alt="profile image" />
         </div>
         <div className="card-content">
           <div className="profile-company-info">
             <h3 className="company-name"></h3>
-            <h3 className="new"></h3>
-            <h3 className="featured"></h3>
+            <h3 className="new">new</h3>
+            <h3 className="featured">featured</h3>
           </div>
           <div className="comapny-job-position">
-            <h2 className="job-title"></h2>
+            <h2 className="job-title">{position}</h2>
           </div>
           <div className="card-conditions">
-            <p></p>
-            <p></p>
-            <p></p>
+            <p>1d ago</p>
+            <p>Part Time</p>
+            <p>Remote</p>
           </div>
         </div>
       </div>
       <div className="card-skills-filter">
-        <p></p>
+        <p>Frontend</p>
+        <p>Senior</p>
+        <p>HTML</p>
       </div>
     </div>
   );
